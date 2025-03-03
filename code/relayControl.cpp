cff1e280789ec822d6d387f70432d41885c012c1
vlrmprjct <mail@lokalnetz.com>
2025-03-03 22:20:19 +0100

    clean up relay control, reverse relay ordering

--- code/relayControl.cpp
+++ code/relayControl.cpp
@@ -1,36 +1,35 @@
 #include "config.h"
 #include <Arduino.h>
 
-uint32_t relaisState = 0; // Aktueller Status aller Relais
+uint32_t relaisState = 0;
 
-void setRelays(uint32_t value)
-{
-    digitalWrite(LATCH, LOW);
-    shiftOut(DATA, CLOCK, MSBFIRST, (value >> 16) & 0xFF);
-    shiftOut(DATA, CLOCK, MSBFIRST, (value >> 8) & 0xFF);
-    shiftOut(DATA, CLOCK, MSBFIRST, value & 0xFF);
-    digitalWrite(LATCH, HIGH);
+void setRelays(uint32_t value) {
+    digitalWrite(RELAY_LATCH, LOW);
+
+    shiftOut(RELAY_DATA, RELAY_CLOCK, MSBFIRST, value & 0xFF);
+    shiftOut(RELAY_DATA, RELAY_CLOCK, MSBFIRST, (value >> 8) & 0xFF);
+    shiftOut(RELAY_DATA, RELAY_CLOCK, MSBFIRST, (value >> 16) & 0xFF);
+
+    digitalWrite(RELAY_LATCH, HIGH);
 }
 
-void setRelay(int relayNumber, bool state)
-{
+void setRelay(int relayNumber, bool state) {
     if (relayNumber < 1 || relayNumber > RELAY_COUNT)
         return;
-    uint32_t mask = (1UL << (relayNumber - 1)); // Maske für das Relais
+    uint32_t mask = (1UL << (relayNumber - 1));
 
-    if (state) {
-        relaisState |= mask; // Relais einschalten (Bit setzen)
+    if (!state) {
+        relaisState |= mask;
     } else {
-        relaisState &= ~mask; // Relais ausschalten (Bit löschen)
+        relaisState &= ~mask;
     }
-    setRelays(relaisState); // Neuen Zustand anwenden
+    setRelays(relaisState);
 }
 
-void toggleRelay(int relayNumber)
-{
+void toggleRelay(int relayNumber) {
     if (relayNumber < 1 || relayNumber > RELAY_COUNT)
         return;
 
-    relaisState ^= (1UL << (relayNumber - 1)); // Relais-Zustand umkehren
+    relaisState ^= (1UL << (relayNumber - 1));
     setRelays(relaisState);
 }
