75abce40d9e994ab6f0f00ad5f7b566de8010aba
vlrmprjct <mail@lokalnetz.com>
2025-04-30 08:33:05 +0200

    add pin comments @ config

--- code/config.h
+++ code/config.h
@@ -3,23 +3,27 @@
 
 #include <Arduino.h>
 
+// LCD DOT MATRIX PINS | LEAVING HERE FOR DOCUMENTATION
+// const int LCD_SCL = 21; // YELLOW | PIN 21 | SCL
+// const int LCD_SDA = 20; // GREEN  | PIN 20 | SDA
+
 // MOTOR MDL 1
-const int MOTOR_IN1 = 6;
-const int MOTOR_IN2 = 7;
+const int MOTOR_IN1 = 6; // YELLOW | PIN 6
+const int MOTOR_IN2 = 7; // GREEN  | PIN 7
 
 // ENCODER MOTOR MDL 1
-const int ENC_MAIN_1_CLK = 18; // INTERRUPT !
-const int ENC_MAIN_1_DT  = 28;
+const int ENC_MAIN_1_CLK = 18; // GREEN  | PIN 18 | INTERRUPT
+const int ENC_MAIN_1_DT  = 28; // YELLOW | PIN 28
 inline int ENC_MAIN_1_CLK_STATE;
 inline int ENC_MAIN_1_DT_STATE;
 inline volatile int ENC_MAIN_1_VALUE = 0;
 
 // RELAIS 74HC595 PINS
-const int RELAY_LATCH = 39; // (ST_CP) YELLOW // 39
-const int RELAY_DATA  = 37; // (DS) GREEN // 37
-const int RELAY_CLOCK = 35; // (SH_CP) BLUE // 35
+const int RELAY_LATCH = 39; // YELLOW | PIN 39 | ST_CP
+const int RELAY_DATA  = 37; // GREEN  | PIN 37 | DS
+const int RELAY_CLOCK = 35; // BLUE   | PIN 35 | SH_CP
 
 // RELAY COUNT
-const int RELAY_COUNT = 24; // Anzahl der Relais
+const int RELAY_COUNT = 24; // RELAY COUNT
 
 #endif
