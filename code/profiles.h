cab6044257ac72e0a590ec6e2d54d9c56157b3be
vlrmprjct <mail@lokalnetz.com>
2025-06-27 11:06:46 +0200

    format profiles

--- code/profiles.h
+++ code/profiles.h
@@ -8,9 +8,18 @@ struct UIDProfile {
 };
 
 inline UIDProfile profiles[] = {
-    { "04 73 86 3A 25 55 80", "Test NFC 1" },
-    { "A2 5E C0 AB", "Test NFC 2" },
-    { "12 1E 90 AB", "Test NFC 3" }
+    {
+        "04 73 86 3A 25 55 80",
+        "Test NFC 1"
+    },
+    {
+        "A2 5E C0 AB",
+        "Test NFC 2"
+    },
+    {
+        "12 1E 90 AB",
+        "Test NFC 3"
+    }
 };
 
 inline const int profileCount = sizeof(profiles) / sizeof(UIDProfile);
