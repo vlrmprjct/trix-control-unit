beadfe19d5d40e0996e6e66d3ad02facaa3f68b6
vlrmprjct <mail@lokalnetz.com>
2025-10-17 15:21:57 +0200

    change servo min max angle

--- code/src/controls/servoControl.h
+++ code/src/controls/servoControl.h
@@ -6,8 +6,8 @@
 namespace ServoControl {
     constexpr int SERVOMIN = 140;
     constexpr int SERVOMAX = 700;
-    constexpr int ANGLE_STRAIGHT  = 60;
-    constexpr int ANGLE_DIVERGING = 130;
+    constexpr int ANGLE_STRAIGHT  = 70;
+    constexpr int ANGLE_DIVERGING = 120;
 
     void switchTurnout(Adafruit_PWMServoDriver& driver, int channel, bool diverging);
     void setAngle(Adafruit_PWMServoDriver& driver, int channel, int angle);
