1dff662f204840e24c0a2861eb52421c5c480e6c
vlrmprjct <mail@lokalnetz.com>
2026-03-15 08:57:56 +0100

    add angel offset to servo control

--- code/src/controls/servoControl.cpp
+++ code/src/controls/servoControl.cpp
@@ -4,13 +4,15 @@
 
 namespace ServoControl {
 
-    void switchTurnout(Adafruit_PWMServoDriver& driver, int channel, bool diverging) {
-        int angle = diverging ? ANGLE_DIVERGING : ANGLE_STRAIGHT;
+    void switchTurnout(Adafruit_PWMServoDriver& driver, int channel, bool diverging, int offset) {
+        int angle = (diverging ? ANGLE_DIVERGING : ANGLE_STRAIGHT) + offset;
+        angle = constrain(angle, 0, 180);
         int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
         driver.setPWM(channel, 0, pulse);
     }
 
     void setAngle(Adafruit_PWMServoDriver& driver, int channel, int angle) {
+        angle = constrain(angle, 0, 180);
         int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
         driver.setPWM(channel, 0, pulse);
     }
--- code/src/controls/servoControl.h
+++ code/src/controls/servoControl.h
@@ -4,12 +4,12 @@
 #include <Adafruit_PWMServoDriver.h>
 
 namespace ServoControl {
-    constexpr int SERVOMIN = 140;
+    constexpr int SERVOMIN = 170;
     constexpr int SERVOMAX = 700;
     constexpr int ANGLE_STRAIGHT  = 70;
     constexpr int ANGLE_DIVERGING = 120;
 
-    void switchTurnout(Adafruit_PWMServoDriver& driver, int channel, bool diverging);
+    void switchTurnout(Adafruit_PWMServoDriver& driver, int channel, bool diverging, int offset = 0);
     void setAngle(Adafruit_PWMServoDriver& driver, int channel, int angle);
 }
 
